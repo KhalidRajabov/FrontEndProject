5808bd712234b6f93444baf97712b54e5c71cd1b
dbBurzum <khalidsr@code.edu.az>
2022-06-04 02:56:41 +0400

    aaaaaaaaaaaa

--- assets/apps/main.js
+++ assets/apps/main.js
@@ -1,6 +1,24 @@
 $(document).ready(function () {
     let btnCategory = document.getElementById("h-drop-down")
+    let shop = document.getElementById("shop-drop")
+    let shopDiv = document.getElementById("shop-div")
+    let homedown = document.getElementById("home-down")
+    let homedrop = document.getElementById("home-drop")
     $("#h-categories").click(function(){
         btnCategory.classList.toggle("hide")
     })
+    shop.onmouseover=function(){
+        shopDiv.classList.remove("hide")
+        shopDiv.classList.add("unhide")
+    }
+    shopDiv.onmouseleave=function(){
+        shopDiv.classList.add("hide")
+        shopDiv.classList.remove("unhide")
+    }
+    // homedown.onmouseover=function(){
+    //     homedrop.classList.remove("hide")
+    // }
+    // homedrop.onmouseleave=function(){
+    //     homedrop.classList.add("hide")
+    // }
 })
