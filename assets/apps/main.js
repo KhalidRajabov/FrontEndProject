dbbed061be74d0c60c3ea936ef4ecaa542ac4191
dbBurzum <khalidsr@code.edu.az>
2022-06-04 01:10:38 +0400

    update

--- assets/apps/main.js
+++ assets/apps/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     let btnCategory = document.getElementById("h-drop-down")
     $("#h-categories").click(function(){
-        btnCategory.classList.add("hide")
+        btnCategory.classList.toggle("hide")
     })
 })
