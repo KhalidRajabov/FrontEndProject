5a2d125a0ffd5f99600b933f8f47f7c933342601
dbBurzum <khalidsr@code.edu.az>
2022-06-08 17:25:18 +0400

    shop/done

--- assets/apps/basket.js
+++ assets/apps/basket.js
@@ -1,13 +1,15 @@
 let cardBtn = document.querySelectorAll(`.card-button`)
-
-let arr= [];
-
-if (localStorage.getItem("basket")==null) {
-    
-}
+let productCount = document.getElementById("product-count")
+window.onload = function() {
+writeProductCount();
+  };
 
 cardBtn.forEach((addBtn) =>{
-   addBtn.addEventListener("click", function(){
+    addBtn.addEventListener("click", function(){
+       if (localStorage.getItem("basket")==null) {
+           localStorage.setItem("basket", JSON.stringify([]))
+       }
+       let arr = JSON.parse(localStorage.getItem("basket"))
        let productId = this.parentElement.parentElement.getAttribute("data-id")
        let existProduct = arr.find(p=>p.id==productId)
        if (existProduct==undefined) {
@@ -22,6 +24,18 @@ cardBtn.forEach((addBtn) =>{
        else{
           existProduct.count++;
        }
+       localStorage.setItem("basket", JSON.stringify(arr))
+       writeProductCount()
        console.log(arr);
    })
 })
+
+
+function writeProductCount(){
+    if (localStorage.getItem("basket")!=null) {
+        let arr = JSON.parse(localStorage.getItem("basket"))
+        productCount.innerText=arr.length
+    }
+
+}
+
