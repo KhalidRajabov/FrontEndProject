edfb7f045f856d69f0ff4c0bd7c643bff2adb9fd
Khalid S. Rajabov <Khalidsr@code.local>
2022-06-11 05:45:31 -0700

    bugs-fixed

--- assets/apps/URLs.js
+++ assets/apps/URLs.js
@@ -7,7 +7,14 @@ $(document).ready(function () {
     $("#blog-list").click(_=>{window.location="blogs.html"})
     $("#contact-list").click(_=>{window.location="contact.html"})
     $("#account").click(_=>{window.location="login&register.html"})
-    
+    $("#profile").click(_=>{window.location="login&register.html"})
+ 
+    let blogs = document.querySelectorAll("#gotoblog")
+    blogs.forEach((event)=>{
+        event.addEventListener("click", ()=>{
+            window.location="blogs.html"
+        })
+    })
 
 
 
